d076c0167a4e5e9f6f1389958eda52a3386b6ee6
cryptocloud9 <duole.hcm@gmail.com>
2024-10-14 20:03:12 +0700

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -19,8 +19,8 @@ const useStyles = makeStyles((theme) => ({
 
 export const About = () => {
   const classes = useStyles();
-  const greetings = "Hello!";
-  const aboutme = `BC Media Entertainment tự hào là đơn vị hàng đầu trong lĩnh vực giải trí và truyền thông kỹ thuật số. Với sứ mệnh kết nối, sáng tạo và mang đến những trải nghiệm giải trí đỉnh cao, chúng tôi không ngừng phát triển và đa dạng hóa các sản phẩm, dịch vụ của mình để đáp ứng nhu cầu ngày càng cao của khách hàng.`;
+  const greetings = "BC Media Entertainment";
+  const aboutme = `Tự hào là đơn vị hàng đầu trong lĩnh vực giải trí và truyền thông kỹ thuật số. Với sứ mệnh kết nối, sáng tạo và mang đến những trải nghiệm giải trí đỉnh cao, chúng tôi không ngừng phát triển và đa dạng hóa các sản phẩm, dịch vụ của mình để đáp ứng nhu cầu ngày càng cao của khách hàng.`;
   return (
     <section id="about">
       <Container component="main" className={classes.main} maxWidth="md">
@@ -43,7 +43,7 @@ export const About = () => {
             </p>
             <a href="#contact" className="contact-btn">
               <i className="fas fa-terminal"></i>
-              <Typography component='span'> Send me a message.</Typography>
+              <Typography component='span'> Send message.</Typography>
             </a>
           </div>
         </div>
