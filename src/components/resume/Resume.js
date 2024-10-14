f017de7ec748690d6ce1cf97ac4bae44175316ac
cryptocloud9 <duole.hcm@gmail.com>
2024-10-14 20:08:13 +0700

    Update Resume.js

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Link } from '@material-ui/core';
 import { TextDecrypt } from '../content/TextDecrypt';
-import ResumePDF from './../../assets/recentprojects/Welcome.svg';
+// import ResumePDF from './../../assets/recentprojects/Welcome.svg';
 import {
   ResumeIcon
 } from '../content/ResumeButton';
 
+const ResumePDF = ""
 const useStyles = makeStyles((theme) => ({
   footerText: {
     position: 'fixed',
